4c6501fd48373dc1287f1e87beb6e7837de58980
Kanoox <tintinbinet@hotmail.fr>
2025-05-07 12:28:26 +0200

    retrait de certain commentaire

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,3 @@
-// import Offers from './components/offer';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Offers />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext';
@@ -31,20 +18,7 @@ import ForgotPassword from './components/forgot-password'
 
 function App() {
   return (
-/*     <Router>
-      <div className="App"> <nav> <Link to="/">Promotions</Link> | <Link to="/categories">Catégories</Link>  | <Link to="/register">Inscription</Link> | <Link to="/login">Connexion</Link> </nav>
-        <Routes>
-          <Route path="/" element={<Offers />} />
-          <Route path="/categories" element={<CategoryList />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/verify-code" element={<VerifyCodeLogin />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-        </Routes>
-      </div>
-    </Router> */
-  
+
   <AuthProvider>
     <Router>
       <Navbar />
